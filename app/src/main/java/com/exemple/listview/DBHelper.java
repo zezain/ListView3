031cce65efba602cc73c0e06831a72a447285ee6
zezain <zezainz@gmail.com>
2017-06-16 01:31:36 +0200

    Initial commit

---
+++ app/src/main/java/com/exemple/listview/DBHelper.java
@@ -0,0 +1,172 @@
+package com.exemple.listview;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.File;
+
+import android.widget.Toast;
+
+public class DBHelper extends SQLiteOpenHelper{
+	
+	public SQLiteDatabase DB;
+	public String DBPath;
+	public static String DBName = "filmotec.db";
+	public static final int version = '1';
+	public static Context currentContext;
+	public static String tableName = "pays";
+	
+
+	public DBHelper(Context context) {
+		super(context, DBName, null, version);
+		currentContext = context;
+
+
+		DBPath = "/data/data/" + context.getPackageName() + "/databases/";
+
+		try {
+			String myPath = DBPath + DBName; // also check the extension of you db file
+			File dbfile = new File(myPath);
+			if (dbfile.exists()) {
+				Toast.makeText(context, "database exists", Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(context, "cant find database", Toast.LENGTH_LONG).show();
+			}
+		}catch(SQLiteException e){
+			System.out.println("Database doesn't exist");
+		}
+		createDatabase();
+
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createDatabase() {
+		boolean dbExists = checkDbExists();
+
+
+		
+		if (dbExists) {
+			// do nothing
+		} else {
+			//DB = currentContext.openOrCreateDatabase(DBName, 0, null);
+//			DB.execSQL("CREATE TABLE IF NOT EXISTS " +
+//        			tableName +
+//        			" (LastName VARCHAR, FirstName VARCHAR," +
+//        			" Country VARCHAR, Age INT(3));");
+//
+//        	DB.execSQL("INSERT INTO " +
+//        			tableName +
+//        			" Values ('M','Sing','India',25);");
+//        	DB.execSQL("INSERT INTO " +
+//        			tableName +
+//        			" Values ('C','Raje','India',25);");
+//        	DB.execSQL("INSERT INTO " +
+//        			tableName +
+//        			" Values ('D','Phonu','Argentina',20);");
+//        	DB.execSQL("INSERT INTO " +
+//        			tableName +
+//        			" Values ('V','Veera','EU',25);");
+//        	DB.execSQL("INSERT INTO " +
+//        			tableName +
+//        			" Values ('T','Shenoi','Bangla',25);");
+//        	DB.execSQL("INSERT INTO " +
+//        			tableName +
+//        			" Values ('L','Lamha','Australia',20);");
+
+			this.getReadableDatabase();
+
+
+
+			try {
+
+				copyDataBase();
+
+			} catch (IOException e) {
+
+				throw new Error("Error copying database");
+
+			}
+
+		}
+	}
+
+	private boolean checkDbExists() {
+		SQLiteDatabase checkDB = null;
+
+		try {
+			String myPath = DBPath + DBName;
+			Log.d("OOOOO",myPath);
+			checkDB = SQLiteDatabase.openDatabase(myPath, null,
+					SQLiteDatabase.OPEN_READONLY);
+
+		} catch (SQLiteException e) {
+
+			// database does't exist yet.
+
+		}
+
+		if (checkDB != null) {
+
+			checkDB.close();
+
+		}
+
+		return checkDB != null ? true : false;
+	}
+
+	/**
+	 * Copies your database from your local assets-folder to the just created empty database in the
+	 * system folder, from where it can be accessed and handled.
+	 * This is done by transfering bytestream.
+	 * */
+	private void copyDataBase() throws IOException {
+
+		//Open your local db as the input stream
+
+		try {
+			InputStream myInput = currentContext.getAssets().open(DBName);
+		} catch (IOException e)
+		{
+			Log.e("PAS DE BDD","PAS DE BDD");
+		}
+		InputStream myInput = currentContext.getAssets().open(DBName);
+		// Path to the just created empty db
+		String outFileName = DBPath + DBName;
+
+		Log.d("NOM DE SORTIE", outFileName);
+
+		//Open the empty db as the output stream
+		OutputStream myOutput = new FileOutputStream(outFileName);
+
+		//transfer bytes from the inputfile to the outputfile
+		byte[] buffer = new byte[1024];
+		int length;
+		while ((length = myInput.read(buffer))>0){
+			myOutput.write(buffer, 0, length);
+		}
+
+		//Close the streams
+		myOutput.flush();
+		myOutput.close();
+		myInput.close();
+
+	}
+}
---
+++ app/src/main/java/com/exemple/listview/DataListView.java
@@ -0,0 +1,73 @@
+package com.exemple.listview;
+
+import java.util.ArrayList;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class DataListView extends ListActivity {
+	
+	private ArrayList<String> results = new ArrayList<String>();
+	private String tableName = DBHelper.tableName;
+	private SQLiteDatabase newDB;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        openAndQueryDatabase();
+        
+        displayResultList();
+        
+        
+    }
+	private void displayResultList() {
+		TextView tView = new TextView(this);
+        tView.setText("This data is retrieved from the database and only 4 " +
+        		"of the results are displayed");
+        getListView().addHeaderView(tView);
+        
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, results));
+        getListView().setTextFilterEnabled(true);
+		
+	}
+	private void openAndQueryDatabase() {
+		try {
+			DBHelper dbHelper = new DBHelper(this.getApplicationContext());
+			newDB = dbHelper.getWritableDatabase();
+
+			Log.d("OOOOOOOOOOOOOOOOOOOOOO","OOOOOOOOOOOOOOOOOOOOOO");
+			Cursor c = newDB.rawQuery("SELECT NumPays, Nom FROM " +
+					tableName , null);
+
+			if (c != null) {
+				if (c.moveToFirst()) {
+					do {
+						String Titre = c.getString(c.getColumnIndex("NumPays"));
+						String Annee = c.getString(c.getColumnIndex("Nom"));
+						results.add("Titre: " + Titre + ", Annee: " + Annee);
+					} while (c.moveToNext());
+				}
+
+	    		c.close();
+	    	}
+
+
+		} catch (SQLiteException se ) {
+        	Log.e(getClass().getSimpleName(), "Could not create or Open the database");
+			Log.d("Prout","Prout");
+       } //finally {
+//        	if (newDB != null)
+//        		newDB.execSQL("DELETE FROM " + tableName);
+//        		newDB.close();
+//        }
+
+	}
+    
+}
