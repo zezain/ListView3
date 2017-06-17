0c05a300cce4054263e87b57cde04e34a200d10c
EliseRoch <elise.rocheron@supelec.fr>
2017-06-17 20:40:23 +0800

    Test ouverture fragment

--- app/src/main/java/com/exemple/listview/BarreDeNavigation.java
+++ app/src/main/java/com/exemple/listview/BarreDeNavigation.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -80,11 +81,13 @@ public class BarreDeNavigation extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         if (id == R.id.nav_camera) {
             Intent intent = new Intent(BarreDeNavigation.this, DataListView.class);
             startActivity(intent);
         } else if (id == R.id.nav_gallery) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new ItemFragment()).commit();
 
         } else if (id == R.id.nav_slideshow) {
 
